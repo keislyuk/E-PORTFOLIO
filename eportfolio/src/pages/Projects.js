6b027b15e63ff8b438e2e0267396c7fb32bb41ab
keislyuk <just.chockie@gmail.com>
2025-11-17 12:14:46 +0800

    First commit

--- eportfolio/src/pages/Projects.js
+++ eportfolio/src/pages/Projects.js
@@ -5,7 +5,16 @@ export default function Projects() {
     <div style={{ padding: "20px" }}>
       <h2>My Projects</h2>
       <ul>
-        <li><strong>Portfolio Website:</strong> Built with React and CSS.</li>
+        <li>
+          <strong>CRUD:</strong> Built with React{" "}
+          <a
+            href="https://reactjs-crud-five.vercel.app/login"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View on Projects
+          </a>
+        </li>
         <li><strong>...:</strong>...</li>
         <li><strong>...:</strong> ...</li>
       </ul>
