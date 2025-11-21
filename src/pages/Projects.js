12e28ee5ead46207efe380fea26f4e86aff8656b
keislyuk <just.chockie@gmail.com>
2025-11-21 13:32:30 +0800

    Your commit message here

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -15,7 +15,18 @@ export default function Projects() {
             View on Projects
           </a>
         </li>
-        <li><strong>...:</strong>...</li>
+        <li><strong>MobApp:</strong>SnackExpo{" "}
+        
+        <a
+            href="https://appetize.io/embed/b_clf3yzcnyyg5af25nlkyh7d7ty?device=pixel9pro&launchUrl=exp%3A%2F%2Fu.expo.dev%2F933fd9c0-1666-11e7-afca-d980795c5824%3Fruntime-version%3Dexposdk%253A54.0.0%26channel-name%3Dproduction%26snack%3D%2540keislyuk%252F08c324%26snack-channel%3D8vnOM9m2go&params=%7B%22EXDevMenuDisableAutoLaunch%22%3Atrue%2C%22EXKernelDisableNuxDefaultsKey%22%3Atrue%7D&appearance=dark&deviceColor=white&scale=auto&orientation=portrait&centered=both"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View on Projects
+          </a>
+        
+        
+        </li>
         <li><strong>...:</strong> ...</li>
       </ul>
     </div>
