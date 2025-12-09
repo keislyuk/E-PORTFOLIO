f0edabb28d4d3f0a3e5cea6e0be50f6c7611d2ba
keislyuk <just.chockie@gmail.com>
2025-12-09 15:44:50 +0800

    Updated About and Project

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,8 +8,33 @@ export default function About() {
         Hi! I'm Keisly
       </p>
       <p>
-        I specialize in Valorant, CS2, ArenaBreakout, R6, Overwatch.
+        Here are my certificates:
       </p>
+
+        <li>
+          <strong>NetworkSupportAndSecurity:</strong> {" "}
+          <a
+            href="https://www.credly.com/badges/66a6a44f-1156-44e0-95b8-461468aa11ba"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View Certificate
+          </a>
+        </li>
+
+        <li>
+          <strong>JavaScriptEssential 1:</strong> {" "}
+          <a
+            href="https://www.credly.com/badges/9fbe849f-aa3f-4ca8-9d78-06636b953b37"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View Certificate
+          </a>
+        </li>
+
+
+
     </div>
   );
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,7 +6,7 @@ export default function Projects() {
       <h2>My Projects</h2>
       <ul>
         <li>
-          <strong>CRUD:</strong> Built with React{" "}
+          <strong>WebCrud:</strong> Built with React{" "}
           <a
             href="https://reactjs-crud-five.vercel.app/login"
             target="_blank"
@@ -15,7 +15,7 @@ export default function Projects() {
             View on Projects
           </a>
         </li>
-        <li><strong>MobApp:</strong>SnackExpo{" "}
+        <li><strong>MobAppCrud:</strong>SnackExpo{" "}
         
         <a
             href="https://appetize.io/embed/b_clf3yzcnyyg5af25nlkyh7d7ty?device=pixel9pro&launchUrl=exp%3A%2F%2Fu.expo.dev%2F933fd9c0-1666-11e7-afca-d980795c5824%3Fruntime-version%3Dexposdk%253A54.0.0%26channel-name%3Dproduction%26snack%3D%2540keislyuk%252F08c324%26snack-channel%3D8vnOM9m2go&params=%7B%22EXDevMenuDisableAutoLaunch%22%3Atrue%2C%22EXKernelDisableNuxDefaultsKey%22%3Atrue%7D&appearance=dark&deviceColor=white&scale=auto&orientation=portrait&centered=both"
@@ -24,10 +24,42 @@ export default function Projects() {
           >
             View on Projects
           </a>
-        
-        
         </li>
-        <li><strong>...:</strong> ...</li>
+
+        <li><strong>ToDoApp'FrontEnd':</strong> ReactJS{" "}
+        
+        <a
+            href="https://todoapp-rdnr.onrender.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View on Projects
+          </a>
+        </li>
+
+<li><strong>ToDoApp:</strong> SnackExpo{" "}
+        
+        <a
+            href="https://appetize.io/embed/b_clf3yzcnyyg5af25nlkyh7d7ty?device=pixel9pro&launchUrl=exp%3A%2F%2Fu.expo.dev%2F933fd9c0-1666-11e7-afca-d980795c5824%3Fruntime-version%3Dexposdk%253A54.0.0%26channel-name%3Dproduction%26snack%3D%2540keislyuk%252Ftodolist%26snack-channel%3DVs0GT8RdcX&params=%7B%22EXDevMenuDisableAutoLaunch%22%3Atrue%2C%22EXKernelDisableNuxDefaultsKey%22%3Atrue%7D&appearance=light&deviceColor=black&scale=auto&orientation=portrait&centered=both"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View on Projects
+          </a>
+        </li>
+
+<li><strong>ToDoAppRepo:</strong> i was struggling with deploying the project with backend so heres the files to the project{" "}
+        
+        <a
+            href="https://github.com/keislyuk/WholeToDoAppFiles"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View on Projects
+          </a>
+        </li>
+
+
       </ul>
     </div>
   );
